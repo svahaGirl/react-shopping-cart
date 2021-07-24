6801751ce10b90a51b1387f0966ecdbc935fb5ff
svahaGirl <fyanton@hotmail.com>
2021-07-24 09:55:40 -0700

    filter-component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import data from "./data.json";
 import Products from './components/Products';
+import Filter from './components/Filter';
 
 class App extends React.Component {
 
@@ -15,6 +16,38 @@ class App extends React.Component {
   }
 
 
+  sortProducts = (event) => {
+    const sort = event.target.value;
+    console.log(event.target.value);
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products.slice().sort((a, b) => 
+      sort === "lowest"
+      ? a.price > b.price 
+        ? 1:-1
+        : sort === "highest"
+        ? a.price < b.price
+        ?  1:-1
+        : a._id < b._id
+        ? 1:-1
+      ),
+    }));
+  };
+
+  filterProducts = (event) => {
+    console.log(event.target.value);
+    if(event.target.value === ""){
+      this.setState({ size: event.target.value, products: data.products });
+    }else{
+      this.setState({
+      size: event.target.value,
+      products: data.products.filter(
+        (product) => product.availableSizes.indexOf(event.target.value)>= 0
+        ),
+      });
+    }            
+  };
+
   render() { 
   return (
     <div className="grid-container">
@@ -22,9 +55,16 @@ class App extends React.Component {
         <a href="/">React Shopping Cart</a>
       
       </header>
-      <main>Product List
+      <main>
         <div className = "content">
           <div className= "main">
+            <Filter count ={ this.state.products.length}>
+              size={ this.state.size}
+              sort={ this.state.sort}
+              filterProducts = { this.filterProducts}
+              sortProducts = { this.sortProducts}
+            </Filter>
+
             <Products products={this.state.products}></Products>
           </div>
           <div className="sidebar">Cart Items</div>
--- src/components/Products.js
+++ src/components/Products.js
@@ -9,9 +9,9 @@ export default class Products extends Component {
             <div>
                 <ul className="products">
                     {this.props.products.map(product => (
-                    <li key ={product.id}>
+                    <li key ={product._id}>
                         <div className="product">
-                            <a href={"#" + product.id}>
+                            <a href={"#" + product._id}>
                             <img src = {product.image} alt={product.title}></img>
                             <p>
                                 {product.title}
